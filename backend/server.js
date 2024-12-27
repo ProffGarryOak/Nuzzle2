25113eb005e0ecca6360392d90f85096806f82ba
ProffGarryOak@gmail.com <you@example.com>
2024-12-27 16:52:06 +0530

    Add new controllers, routes, and models for social media app

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,47 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes.js';
-import dotenv from 'dotenv';
-dotenv.config();
-const app = express();
-app.use('/api/auth', authRoutes);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
